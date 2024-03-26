60f7f2b970cf6d6c77e1c2b7f2a31e1f2e9450b3
Krishnendu Bhattacharyya <krishnendu19802@gmail.com>
2024-03-26 15:40:52 +0530

    doctor page

---
+++ frontend/src/Components/Doctor/resp.jsx
@@ -0,0 +1,145 @@
+const booking={
+    "slots": [
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": true,
+                "content": {
+                    "patient_email": "user1@gmail.com",
+                    "date": "2024-03-26T00:00:00.000Z",
+                    "slot": 1
+                }
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": true,
+                "content": {
+                    "patient_email": "user3@gmail.com",
+                    "date": "2024-03-26T00:00:00.000Z",
+                    "slot": 3
+                }
+            }
+        ],
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": true,
+                "content": {
+                    "patient_email": "user2@gmail.com",
+                    "date": "2024-03-27T00:00:00.000Z",
+                    "slot": 2
+                }
+            },
+            {
+                "status": false,
+                "content": {}
+            }
+        ],
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            }
+        ],
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            }
+        ],
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            }
+        ],
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            }
+        ],
+        [
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            },
+            {
+                "status": false,
+                "content": {}
+            }
+        ]
+    ],
+    "start_time": 9
+}
+
+export default booking
