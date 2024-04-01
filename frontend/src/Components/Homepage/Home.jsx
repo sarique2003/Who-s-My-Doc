178ee99c479981890573323284a0afa33e74abaf
Mohd Sarique <shaarik.aslam@gmail.com>
2024-04-01 10:56:33 +0530

    Some chnages to Homepage

--- frontend/src/Components/Homepage/Home.jsx
+++ frontend/src/Components/Homepage/Home.jsx
@@ -14,45 +14,66 @@ import { AuthContext } from "../../context/AuthProvider";
 import Footer from "../Footer/Footer";
 function Home() {
   const navigate = useNavigate();
-  const { isAuthenticated } = useContext(AuthContext)
+  const { isAuthenticated } = useContext(AuthContext);
   useEffect(() => {
     // if (isAuthenticated[0] === false)
     // navigate('/login')
-  }, [isAuthenticated])
+  }, [isAuthenticated]);
   return (
     <div className="container-main-home">
       <NavBar />
       <div className="main-text">
         <div className="main-text-left">
+          <text className="mt-8">
+            {isAuthenticated[0] === false ||
+            isAuthenticated[1].type === "patient"
+              ? "Your health our priority"
+              : "Welcome Doctor ..."}{" "}
+          </text>
 
-
-          <text className="mt-8">{isAuthenticated[0] === false || isAuthenticated[1].type === 'patient' ? "Your health our priority" : "Welcome Doctor ..."} </text>
-
-
-          <p class="healthcare-message" style={{ textAlign: 'justify', paddingLeft: "20px", paddingTop: '15px' }}>
-            We Sarique, Abhik, Krishnendu, Ayush came together to create this
+          <p
+            class="healthcare-message"
+            style={{
+              textAlign: "justify",
+              paddingLeft: "20px",
+              paddingTop: "15px",
+            }}
+          >
+            We Abhik, Krishnendu, Sarique ,Ayush came together to create this
             healthcare management system,driven by our passion for improving
             healthcare management.Every year about 8% healthcare tragedies occur
-            in our nation because of the inability of the population to reach the
-            correct doctor for normally treatable health ailments.Through our
-            collective efforts and utilisation of our DataBase Managmenet and
-            development skills, we aim to make a positive impact on people's lives
-            by providing innovative solutions and compassionate care.
+            in our nation because of the inability of the population to reach
+            the correct doctor for normally treatable health ailments.Through
+            our collective efforts and utilisation of our DataBase Managmenet
+            and development skills, we aim to make a positive impact on people's
+            lives by providing innovative solutions and compassionate care.
           </p>
 
-          <button id="btn-doc-find" style={{ marginLeft: '20px' }}
-            onClick={() => { isAuthenticated[0] === false || isAuthenticated[1].type === 'patient' ? navigate("/patient") : navigate("/doctor-booking-history") }}
-          >{isAuthenticated[0] === false || isAuthenticated[1].type === 'patient' ? "Find Your Doctor" : "View Your Patients"}</button>
+          <button
+            id="btn-doc-find"
+            style={{ marginLeft: "20px" }}
+            onClick={() => {
+              isAuthenticated[0] === false ||
+              isAuthenticated[1].type === "patient"
+                ? navigate("/patient")
+                : navigate("/doctor-booking-history");
+            }}
+          >
+            {isAuthenticated[0] === false ||
+            isAuthenticated[1].type === "patient"
+              ? "Find Your Doctor"
+              : "View Your Patients"}
+          </button>
         </div>
 
         <div class="image-container">
-          <img src={img05} className="shadow" alt="Doctor 1" />
-
+          <img src={img05} className="shadow mx-4" alt="Doctor 1" />
+          <img src={img03} className="shadow mx-5 img-2" alt="Doctor 2" />
         </div>
       </div>
 
       <Footer />
-    </div >
+    </div>
   );
 }
 
--- frontend/src/Components/Navbar/NavBar.jsx
+++ frontend/src/Components/Navbar/NavBar.jsx
@@ -1,50 +1,127 @@
-import React, { useContext, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../context/AuthProvider';
+import React, { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
+import "./NavBar.css";
+import logo1black from "../../assets/logo1black.png";
 
-export default function Navbar() {
+export default function NavBar() {
+  const { isAuthenticated, login, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const type = isAuthenticated[0] && isAuthenticated[1].type;
+  console.log(type);
+  const handlelogout = () => {
+    logout();
+    localStorage.removeItem("whosmydoc");
+    navigate("/login");
+  };
+  useEffect(() => {
+    // if (isAuthenticated[0] === false)
+    // navigate('/login')
+  }, [isAuthenticated]);
 
-    const { isAuthenticated, login, logout } = useContext(AuthContext);
-    const navigate = useNavigate()
+  return (
+    <div>
+      {
+        // isAuthenticated[0] &&
+        <nav
+          className="navbar navbar-expand-lg bg-body-tertiary"
+          style={{ padding: "0px" }}
+        >
+          <div className="container-fluid">
+            <Link className="navbar-brand me-auto" to="/">
+              <img
+                src={logo1black}
+                style={{ height: "45px", width: "auto", borderRadius: "5px" }}
+              />
+            </Link>
+            <button
+              className="navbar-toggler "
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse " id="navbarNav">
+              <ul className="navbar-nav" style={{ width: "100%" }}>
+                {isAuthenticated[0] && (
+                  <>
+                    <li className="nav-item ms-auto">
+                      {isAuthenticated[1].type === "patient" ? (
+                        <Link
+                          className="nav-link active"
+                          aria-current="page"
+                          to="/patient"
+                        >
+                          Book Doctor
+                        </Link>
+                      ) : (
+                        <Link
+                          className="nav-link active"
+                          aria-current="page"
+                          to="/doctor"
+                        >
+                          Booking Log
+                        </Link>
+                      )}
+                    </li>
+                    <li className="nav-item">
+                      <Link
+                        className="nav-link active"
+                        aria-current="page"
+                        to={
+                          isAuthenticated[1].type === "patient"
+                            ? "/patient-profile"
+                            : "/doctor-booking-history"
+                        }
+                      >
+                        Previous Records
+                      </Link>
+                    </li>
+                    <li className="nav-item me-auto">
+                      <Link
+                        className="nav-link active"
+                        to={`${
+                          type === "doctor"
+                            ? "/doctor-user-prof"
+                            : "/patient-user-prof"
+                        }`}
+                      >
+                        User Details
+                      </Link>
+                    </li>
+                    <li className="nav-item ">
+                      <Link className="nav-link" to="#">
+                        {isAuthenticated[1]?.name}
+                      </Link>
+                    </li>
+                  </>
+                )}
 
-    const handlelogout = () => {
-        logout()
-        navigate('/login')
-    }
-    useEffect(()=>{
-        if(isAuthenticated[0]===false)
-        navigate('/login')
-    },[isAuthenticated])
-
-    return (
-        <div>
-            {isAuthenticated[0] &&<nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                <li
+                  className={`nav-item ${!isAuthenticated[0] ? "ms-auto" : ""}`}
+                >
+                  {isAuthenticated[0] ? (
+                    <button className="btn btn-danger" onClick={handlelogout}>
+                      Logout
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav" style={{width:'100%'}}>
-                            <li className="nav-item ms-auto">
-                                <Link className="nav-link active" aria-current="page" href="#">Book Doctor</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" href="#">Previous Records</Link>
-                            </li>
-                            <li className="nav-item me-auto">
-                                <Link className="nav-link" href="#">User Details</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#">{isAuthenticated[1].name}</Link>
-                            </li>
-                            <li className="nav-item">
-                                <button className="btn btn-danger" onClick={handlelogout}>Logout</button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>}
-        </div>
-    )
+                  ) : (
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => navigate("/login")}
+                    >
+                      Login
+                    </button>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      }
+    </div>
+  );
 }
